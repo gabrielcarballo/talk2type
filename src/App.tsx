53901bb5608821ae7b6666733057c7f97553b272
gabrielcarballo <gabrielfpcarballo@gmail.com>
2024-02-10 20:54:21 +0100

    feat: Add Modal to NoteCard Added Radix Dialog to add modal behavior to NoteCard component when clicked

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import logo from './assets/logo-nlw.svg';
 import { NewNoteCard } from './components/NewNoteCard';
 import { NoteCard } from './components/NoteCard';
 
+const note = {
+  content: 'texto 1',
+  date: new Date()
+}
+const note2 = {
+  content: 'texto 2',
+  date: new Date()
+}
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -18,8 +27,8 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note}/>
+        <NoteCard note={note2}/>
       </div>
     </div>
   )
--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,11 +1,41 @@
-// interface NoteCardProps {}
+import * as Dialog from '@radix-ui/react-dialog'
+import { formatDistanceToNow } from 'date-fns'
+import { X } from 'lucide-react'
 
-export const NoteCard = () => {
+interface NoteCardProps {
+  note:
+  {
+    content: string;
+    date: Date;
+  }
+}
+
+export const NoteCard = ({ note }: NoteCardProps) => {
   return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 outline-none hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-          <span className='text-sm font-medium text-slate-300'>Add Note</span>
-          <p className='text-sm leading-6 text-slate-400'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus distinctio nulla ratione debitis, dolor in aut repudiandae aperiam blanditiis at. Odio eligendi dolores iste aperiam praesentium, illum labore placeat modi.</p>
-          <div className="absolute bottom-0 right-0 left-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </button>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 outline-none hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className='text-sm font-medium text-slate-300'>{note.date?.toISOString()} days ago</span>
+        <p className='text-sm leading-6 text-slate-400'>{note.content}</p>
+        <div className="absolute bottom-0 right-0 left-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/60'>
+          <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none'>
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className='text-sm font-medium text-slate-300'>{formatDistanceToNow(note.date, { addSuffix: true })}</span>
+              <p className='text-sm leading-6 text-slate-400'>{note.content}</p>
+            </div>
+            <button
+              className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'
+              type="button">
+              <span className="text-red-400 group-hover:underline">Delete this note?</span>
+            </button>
+            <Dialog.Close className='absolute top-5 right-5 text-slate-300 hover:text-slate-100'>
+              <X className='size-5 hover:text-slate-100'/>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
